60c9acc414ee061ce94585898bccddba840f6b4d
kristoffer <kristoffer@teletronics.ae>
2024-08-01 19:10:42 +0400

    Update GIO settins

--- dose/pump.py
+++ dose/pump.py
@@ -7,30 +7,30 @@ class Pump():
         GPIO.setup(PIN_E, GPIO.OUT)
         GPIO.setup(PIN_M, GPIO.OUT)
             
-        GPIO.output(PIN_E, GPIO.HIGH)
-        GPIO.output(PIN_M, GPIO.HIGH)
+        GPIO.output(PIN_E, GPIO.LOW)
+        GPIO.output(PIN_M, GPIO.LOW)
         self.p = GPIO.PWM(PIN_M, 1000)
         self.p.start(0)
 
-        self.current = GPIO.HIGH
+        self.current = GPIO.LOW
         self.control_pin = PIN_E
         
     def flow(self):
-        if self.current == GPIO.LOW:
-            return
-       
-        GPIO.output(self.control_pin, GPIO.LOW)   
-        self.p.ChangeDutyCycle(80)
-        
-        self.current = GPIO.LOW
-
-    def stop(self):
         if self.current == GPIO.HIGH:
             return
+       
+        GPIO.output(self.control_pin, GPIO.HIGH)   
+        self.p.ChangeDutyCycle(80)
         
-        GPIO.output(self.control_pin, GPIO.HIGH)
-        self.p.ChangeDutyCycle(0)
         self.current = GPIO.HIGH
+
+    def stop(self):
+        if self.current == GPIO.LOW:
+            return
+        
+        GPIO.output(self.control_pin, GPIO.LOW)
+        self.p.ChangeDutyCycle(0)
+        self.current = GPIO.LOW
        
     def shutdown(self):
         GPIO.cleanup()
