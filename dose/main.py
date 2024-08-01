1079d006e7d7b85d16d671540afa65b83951358b
kristoffer <kristoffer@teletronics.ae>
2024-08-01 18:52:41 +0400

    Adjusting to multiple motors

--- dose/main.py
+++ dose/main.py
@@ -7,11 +7,14 @@ from std_msgs.msg import String
 import time
 from datetime import datetime
 
-PIN_IN1 = int(os.getenv('PIN_IN1',22))
-PIN_IN2 = int(os.getenv('PIN_IN2',23))
-PIN_EN = int(os.getenv('PIN_EN',23))
+PIN_PUMP1_E = int(os.getenv('PIN_PUMP1_E',0))
+PIN_PUMP1_M = int(os.getenv('PIN_PUMP1_M',0))
 
-TARGET_PH = float(os.getenv('TARGET_PH', 5.8))
+PIN_PUMP2_E = int(os.getenv('PIN_PUMP2_IN1',0))
+PIN_PUMP2_M = int(os.getenv('PIN_PUMP2_IN2',0))
+
+TARGET_METRIC = float(os.getenv('TARGET_METRIC', 'ph'))
+TARGET_VALUE = float(os.getenv('TARGET_VALUE', 7.0))
 
 DOSE_INTERVAL = int(os.getenv('DOSE_INTERVAL', 5))
 
@@ -25,12 +28,15 @@ class Dose(Node):
     def __init__(self):
         super().__init__('dose')
         self.get_logger().info('Startig dose pin in1="%d", in2="%d", en="%d" and target ph of "%d"' % (PIN_IN1, PIN_IN2, PIN_EN, TARGET_PH))
-        self.pump = pump.Pump(PIN_IN1, PIN_IN2, PIN_EN)
+        self.pump1 = pump.Pump(PIN_PUMP1_E, PIN_PUMP1_M)
+        if PIN_PUMP2_E > 0:
+            self.pump2 = pump.Pump(PIN_PUMP2_E, PIN_PUMP2_M)
+       
         self.schedule = NIGHT
 
         self.subscription = self.create_subscription(
             Float32,
-            'ph',
+            TARGET_METRIC,
             self.listener_callback,
             10)
         
@@ -45,16 +51,16 @@ class Dose(Node):
         self.schedule = msg.data
         
     def listener_callback(self, msg):
-        self.get_logger().info('Getting ph value of: "%.2f"' % msg.data)
+        self.get_logger().info('Getting value of: "%.2f"' % msg.data)
         
         if self.schedule == NIGHT:
             self.get_logger().info('No dose - schedule is night')
             return
 
-        ph = msg.data
+        value = msg.data
 
-        if ph <= TARGET_PH:
-            self.get_logger().info('No dose - ph value less than target')
+        if value <= TARGET_VALUE:
+            self.get_logger().info('No dose - value less than target')
             return 
         
         try:
@@ -69,9 +75,15 @@ class Dose(Node):
             self.get_logger().info('First run. Setting last_run')
             self.last_run = datetime.now()
         
-        self.pump.flow()
+        self.pump1.flow()
+        if self.pump2:
+            self.pump2.flow()
+
         time.sleep(DOSE_INTERVAL)
         self.pump.stop()
+        if self.pump2:
+            self.pump2.stop()
+        
         self.get_logger().info('Dose given. Last run was: "%s"' % self.last_run.strftime('%Y-%m-%d %H:%M:%S'))                
         self.last_run = datetime.now()
         
--- dose/pump.py
+++ dose/pump.py
@@ -1,20 +1,19 @@
 import RPi.GPIO as GPIO
 
 class Pump():
-    def __init__(self, PIN_IN1, PIN_IN2, PIN_EN):
+    def __init__(self, PIN_E, PIN_M):
         
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PIN_IN1, GPIO.OUT)
-        GPIO.setup(PIN_IN2, GPIO.OUT)
-        GPIO.setup(PIN_EN, GPIO.OUT)
-        
-        GPIO.output(PIN_IN1, GPIO.HIGH)
-        GPIO.output(PIN_IN2, GPIO.HIGH)
-        self.p = GPIO.PWM(PIN_EN, 1000)
+        GPIO.setup(PIN_E, GPIO.OUT)
+        GPIO.setup(PIN_M, GPIO.OUT)
+            
+        GPIO.output(PIN_E, GPIO.HIGH)
+        GPIO.output(PIN_M, GPIO.HIGH)
+        self.p = GPIO.PWM(PIN_M, 1000)
         self.p.start(0)
 
         self.current = GPIO.HIGH
-        self.control_pin = PIN_IN1
+        self.control_pin = PIN_E
         
     def flow(self):
         if self.current == GPIO.LOW:
